6cae0ddb6693b11da798324bcf113163b14af0fc
Andreas Nilsson <andreas.nilsson@hkr.se>
2024-11-14 10:17:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import csv
+
+
+def read_csv(filename):
+    raw_data = dict()
+    with open(filename, 'r') as in_file:
+        reader = csv.reader(in_file, quotechar='"', delimiter=',', skipinitialspace=True)
+        for row in reader:
+            raw_data[row[0]] = row[1:]
+    return raw_data
+
+
+def read_names(filename):
+    with open(filename, 'r') as in_file:
+        return [name.strip() for name in in_file]
+
+
+def write_csv(filename, data, include_names=True):
+    with open(filename, 'w') as out_file:
+        writer = csv.writer(out_file, quotechar='"', delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        for name, values in data.items():
+            if include_names:
+                writer.writerow([name] + values)
+            else:
+                writer.writerow(values)
+
+
+def replace(data, index, condition, new_value):
+    for key, values in data.items():
+        if condition(values[index]):
+            values[index] = new_value
+
+
+def main():
+    names = read_names('names.txt')
+    raw_data = read_csv('export.csv')
+    columns = [6]  # 0 is the column AFTER the name column
+    filtered_data = {name: [raw_data[name][i] for i in columns] for name in names}
+
+    replace(filtered_data, 0, lambda x: x == '0.00', 'nok')
+    replace(filtered_data, 0, lambda x: x == '1.00', 'ok')
+    # replace(filtered_data, 1, lambda x: x == '0.00', 'nok')
+    # replace(filtered_data, 1, lambda x: x == '0.75', '4')
+    # replace(filtered_data, 1, lambda x: x == '1.00', '5')
+
+    write_csv('output.csv', filtered_data, include_names=False)
+
+
+if __name__ == '__main__':
+    main()
